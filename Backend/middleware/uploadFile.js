541920991c4068e23b4407b27bf01fd1898d7170
Subodh-create <shahsubodh624@gmail.com>
2022-03-20 16:20:38 +0545

    testing

--- Backend/middleware/uploadFile.js
+++ Backend/middleware/uploadFile.js
@@ -5,7 +5,17 @@ const router = require("../routes/doctor");
 
 
 
+const Storage = multer.diskStorage({
+  /////where to storage our file
 
+  //where and with what name (foldername,filename)
+  destination: function (req, file, cb) {
+    cb(null, "./medicinePic"); //where jobs is foldername
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
 
 
